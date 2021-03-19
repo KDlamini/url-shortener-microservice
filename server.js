a64239e5aa9addef460e8a28a63591597e305ea6
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-19 13:10:42 +0200

    added body-parser, mongoose, mongodb and created url schema

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +24,19 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urlSchema = new Schema({
+  original: { type: String, required: true },
+  short: Number
+})
+
+const Url = mongoose.model('Url', urlSchema);
+
+app.post('api/shorturl/new', bodyParser.urlencoded({extended: false}) , (req, res) => {
+  
+  console.log(req.body);
+  res.json({})
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
