c990bd8fc78c119edf894c32801b3b61db42dccc
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-19 13:19:29 +0200

    updated env file info as well mongoose connection path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 //database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
