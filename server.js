fb39f1609ece28eeca1b5bd921613aab26e399d2
Simo Dlamini <wilzmar.kwd@gmail.com>
2021-03-20 11:45:50 +0000

    added url validator, mongoose model, get request and saving documents to atlas

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
+const validate = require('url-validator');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const app = express();
 
 //database connection
 let uri = mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -26,16 +28,49 @@ app.get('/api/hello', function(req, res) {
 });
 
 let urlSchema = new Schema({
-  original: { type: String, required: true },
-  short: Number
+  original_url: { type: String, required: true },
+  short_url: String
 })
 
 const Url = mongoose.model('Url', urlSchema);
 
 app.post('/api/shorturl/new', bodyParser.urlencoded({extended: false}) , (req, res) => {
-  res.json({
-    "original_url": req.body["url"],
-    "short_url": 1
+  let requested_url = validate(req.body.url);
+
+  if(requested_url) {
+
+    let suffix = shortid.generate();
+    let shorturl = suffix;
+
+    let url_document = new Url({
+      original_url: requested_url,
+      short_url: shorturl
+    });
+
+    url_document.save((error, doc) => {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log("Successfuly saved document!")
+        res.json({
+          original_url: doc.original_url,
+          short_url: doc.short_url
+        })
+      }
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.get('/api/shorturl/:short', (req, res) => {
+   
+  Url.findOne({short_url: req.params.short}, (error, data) => {
+    if(error) {
+      console.log(error);
+    } else {
+      res.redirect(data.original_url);
+    }
   })
 })
 
